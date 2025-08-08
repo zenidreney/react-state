ab87f6024f379698f0cf57f45dfd7eca7a5cc38d
Deniz <auo@tuta.io>
2025-08-08 19:40:40 +0200

    built the first draft

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+    
+        <header>
+            
+            <h1>Draw on a custom pic</h1>
+            
+        </header>
+        
+    )
+}
+
+export default Header
---
+++ src/assets/components/Main.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function Main() {
+    const [imgUrl, setImgUrl] = React.useState("https://picsum.photos/200/300");
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    //console.log("Main Rendering");
+
+    function getImage() {
+        setIsLoading(true);
+
+        fetch("https://picsum.photos/v2/list?page=2&limit=100")
+            .then((res) => res.json())
+            .then((data) => {
+                const rI = Math.floor(Math.random() * data.length);
+                setImgUrl(data[rI].download_url);
+                setIsLoading(false);
+            })
+        .catch(err => {
+            console.error("Failed to get image!", err);
+            setIsLoading(false)
+        })
+        ;
+    }
+
+    React.useEffect(function () {
+        getImage();
+    }, []);
+
+    return (
+        <main>
+            <button onClick={getImage}>Change Image</button>
+            {isLoading && <p>LOADING</p>}
+            <img src={imgUrl} />
+        </main>
+    );
+}
+
+export default Main;
