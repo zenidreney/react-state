edd2a04b5df5fe4832ed0c93cf9fcc9a4b83ce0f
Deniz <auo@tuta.io>
2025-08-11 16:34:31 +0200

    refactored code

--- src/assets/components/Game.jsx
+++ src/assets/components/Game.jsx
@@ -1,45 +1,41 @@
-import { useState } from "react"
+import { useState } from "react";
 import Color from "./Color";
-import { nanoid } from "nanoid"
-
+import { nanoid } from "nanoid";
 
 function Game() {
+    const [colorsArray, setColorsArray] = useState(() =>
+        ["blue", "red", "black", "white", "green", "pink", "darkviolet", "khaki"].map((color) => {
+            return {
+                id: nanoid(),
+                colorValue: color
+            };
+        })
+    );
     
-    const [colorsArray, setColorsArray] = useState(["blue", "red", "black", "white", "green", "pink", "darkviolet", "khaki"]);
-    
-    const colorBtns = colorsArray.map(color => (
-        <Color key={nanoid()} id={nanoid()} color={color} />
-    ))
-    
+    //console.log(colorsArray)
+
+    const colorBtns = colorsArray.map((color) => <Color key={color.id} id={color.id} color={color.colorValue} />);
+
     function mixColors() {
-        //console.log("Mixey Mixey")
-        
-        const copyArray = [...colorsArray]
-        //console.log(copyArray)
-        
-        for (let i = copyArray.length -1; i > 0; i--) {
-            //console.log("I am iterating");
+        const copyArray = [...colorsArray];
+
+        for (let i = copyArray.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            //console.log([copyArray[i], copyArray[j]] = [copyArray[j], copyArray[i]])
             [copyArray[i], copyArray[j]] = [copyArray[j], copyArray[i]];
         }
-        console.log(copyArray)
-        setColorsArray(copyArray)
+        console.log(copyArray);
+        setColorsArray(copyArray);
     }
 
-    
-    
     return (
         <article>
-           
             <h2>Choose the same colors</h2>
 
-            <div className="color-container">
-                {colorBtns} 
-            </div>
-            
-            <button className="mix-button" onClick={mixColors} >Mix Colors</button>
-            
+            <div className="color-container">{colorBtns}</div>
+
+            <button className="mix-button" onClick={mixColors}>
+                Mix Colors
+            </button>
         </article>
     );
 }
